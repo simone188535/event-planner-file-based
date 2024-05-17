e83cb3bb9dc8c57781bfaa932e6c80b0659b50f3
Simone Anthony <s.anthony@tier1performance.com>
2024-05-17 17:13:43 -0400

    FormTextInput component helper created, takes mui components

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,5 +1,63 @@
+import { useForm, useController, Control } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup"
+
+import { Container, Box, TextField, Button, TextFieldProps } from '@mui/material';
+
+
+type FormValues = {
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+}
+
+const schema = yup
+    .object({
+        username: yup.string().required(),
+        email: yup.string().email().required(),
+        password: yup.string().min(5).max(25).required(),
+        passwordConfirm: yup.string().min(5).max(25).required(),
+
+    })
+    .required()
+
+type IFormTextInput = {
+    name: string;
+    control: Control<any>;
+} & TextFieldProps;
+
+
+function FormTextInput({ name, control, ...props }: IFormTextInput) {
+    const {
+        field
+    } = useController({
+        name,
+        control,
+    });
+
+    return <TextField id="outlined-basic" label={name} {...field} {...props} />
+
+}
 export default function Signup() {
-    return <main>
+    const { handleSubmit, control } = useForm<FormValues>({resolver: yupResolver(schema)});
+
+
+    const onSubmit = (data: FormValues) => console.log(data);
+
+    return (<Container component="main">
         Signup Page
-        </main>
+        <Box
+            component="form"
+            onSubmit={
+                handleSubmit(onSubmit)
+            }
+        >
+            <FormTextInput name="Username" control={control}  variant='outlined' sx={{}}/>
+            <FormTextInput name="Email" control={control}  variant='outlined' />
+            <FormTextInput name="Password" control={control}  variant='outlined' />
+            <FormTextInput name="Confirm Password" control={control}  variant='outlined' />
+            <Button variant="contained" type="submit">Submit</Button>
+        </Box>
+    </Container>)
 }
