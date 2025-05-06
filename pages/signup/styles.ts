5dfd8b77767b4c64eebe01a255fc0a5403dd2d3d
Simone Anthony <s.anthony@tier1performance.com>
2025-05-06 13:59:28 -0400

    signup import corrections

--- pages/signup/styles.ts
+++ pages/signup/styles.ts
@@ -1,6 +1,12 @@
-export const signupFormContainer = {
+ const signupFormContainer = {
     display: 'flex',
     flexDirection: 'column',
     // add more later
 }
 
+const formPageContainer = {
+    mt: '50px'
+}
+
+
+export { signupFormContainer, formPageContainer };
